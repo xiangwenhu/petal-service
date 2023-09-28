a8f5b05921dde63ebd4d141bf753e896ffdf259b
huxw <huxw@mail.jj.cn>
2023-09-28 23:41:38 +0800

    调整输出

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
+import createInstance from "./createInstance";
+import BaseService from "./BaseService";
 
-export * from "./defaultInstance";
-export { default as BaseService } from "./BaseService"
+export {
+    createInstance,
+    BaseService
+}
+export * from "./types";
+
+
+// 默认实例导出
+const instance = createInstance();
+export const classDecorator = instance.classDecorator;
+export const createDecorator = instance.createDecorator;
+export const fieldDecorator = instance.fieldDecorator;
+export const methodDecorator = instance.methodDecorator;
+export const paramsDecorator = instance.paramsDecorator;
+export const setConfig = instance.setConfig;
+
+
+// 全局属性申明
+declare global {
+    namespace globalThis {
+        const petalClassDecorator: typeof instance.classDecorator;
+        const petalCreateDecorator: typeof instance.createDecorator;
+        const petalFieldDecorator: typeof instance.fieldDecorator;
+        const petalMethodDecorator: typeof instance.methodDecorator;
+        const petalParamsDecorator: typeof instance.paramsDecorator;
+
+        const setPetalConfig: typeof instance.setConfig;
+
+        const createPetalInstance: typeof createInstance;
+
+        const BasePetalService: typeof BaseService;
+    }
+}
+// 全局属性设置
+var g = globalThis as any;
+g.petalClassDecorator = instance.classDecorator;
+g.petalCreateDecorator = instance.createDecorator;
+g.petalFieldDecorator = instance.fieldDecorator;
+g.petalMethodDecorator = instance.methodDecorator;
+g.petalParamsDecorator = instance.paramsDecorator;
+
+g.setPetalConfig = instance.setConfig;
+
+g.createPetalInstance = createInstance;
+
+g.BasePetalService = BaseService;
--- test/manual/baseService.ts
+++ test/manual/baseService.ts
@@ -1,4 +1,4 @@
-import {  BaseService, classDecorator, methodDecorator, setConfig, fieldDecorator } from "../../src";
+import { BaseService, classDecorator, methodDecorator, setConfig, fieldDecorator } from "../../src";
 import { RequestConfig } from "../../src/types";
 
 // 更新配置，比如授权信息，例如jwt, cookies
